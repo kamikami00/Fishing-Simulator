658dee1bf8c9d886be5e459072b2207b12223875
CocoTheOwner <65736624+CocoTheOwner@users.noreply.github.com>
2020-06-05 00:23:17 +0200

    Update

---
+++ Fishing Simulater 0.1/Assets/sex.cs
@@ -0,0 +1,19 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class sex : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[,];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
